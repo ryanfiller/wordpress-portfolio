62234559cc6e27083099f4315554056dbaf8f532
Ryan <ryan@gorocketfuel.com>
2016-10-01 21:39:04 -0500

    adds swiper js files

---
+++ portfolio-template.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Portfolio Page
+ * Description: A portfolio page with a swiper page banner
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+Timber::render( array( 'page-' . $post->post_name . '.twig', 'page-portfolio.twig' ), $context );
